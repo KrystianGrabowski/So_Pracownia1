80a7964d2d5812e33ff28fe2c5c3623ea2a0c8af
KrystainGrabowski <krystian987543@gmail.com>
2017-10-21 18:50:05 +0200

    Hello v2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <pthread.h>
+#include <stdio.h>
 
 void *PrintHello(void *threadid) {
 	int *l = (int*)threadid;
 	int tID = *l;
 	printf("Hello World! Thread ID: %d\n", *l);
+}
+
+#define NUM 8
+
+int main() {
+	int i = 0;
+	pthread_t tab[NUM];
+	for(i = 0; i < NUM; i++) {
+		pthread_create(&tab[i], NULL, PrintHello, &i);
+	}
+	
+	for(i = 0; i < NUM; i++) {
+		pthread_join(tab[i], NULL);
+	}
+	
+	return 0;
 }
